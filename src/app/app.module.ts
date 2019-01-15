37202e765383f27c40c882a4a5c8fe6c47755191
Phillip Byrd <Pbyrd@oakwood.com>
2019-01-15 09:48:54 -0800

    Completed the first App assignment. Created 4 message components and nested them to show different outputs to the user

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,20 @@ import{HttpModule} from '@angular/http';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { MessageComponent } from './message/message.component';
+import { MessagesComponent } from './messages/messages.component';
+import { WarningMessageComponent } from './message/warning-message/warning-message.component';
+import { SuccessMessageComponent } from './message/success-message/success-message.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    MessageComponent,
+    MessagesComponent,
+    WarningMessageComponent,
+    SuccessMessageComponent
   ],
   imports: [
     BrowserModule,
