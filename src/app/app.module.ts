808aaf6127d48c832f8bfa9f31017465e77e0746
PhillByrd2018 <Phill.e.byrd@gmail.com>
2019-02-11 08:57:33 -0800

    Working through the directive section. We are adding styling and events through the directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,9 @@ import { DetailsComponent } from './details/details.component';
 import { GameControlComponent } from './game-control/game-control.component';
 import { OddComponent } from './odd/odd.component';
 import { EvenComponent } from './even/even.component';
+import { BasicHighlightDirective } from './basic-highlight/basic-highlight-directive';
+import { BetterHighlightDirective } from './better-highlight/better-highlight.directive';
+
 
 @NgModule({
   declarations: [
@@ -28,7 +31,9 @@ import { EvenComponent } from './even/even.component';
     DetailsComponent,
     GameControlComponent,
     OddComponent,
-    EvenComponent
+    EvenComponent,
+    BasicHighlightDirective,
+    BetterHighlightDirective
   ],
   imports: [
     BrowserModule,
